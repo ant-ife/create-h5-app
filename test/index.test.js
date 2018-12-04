74a74e77dc761f89a9732a0b572eb36a01e3d99c
Yuheng Zhang <zhangyuheng91@gmail.com>
2018-12-04 13:26:58 +0800

    fix: package

--- test/index.test.js
+++ test/index.test.js
@@ -1,32 +1,32 @@
-import path from 'path';
-import test from 'ava';
-import sao from 'sao';
+import path from 'path'
+import test from 'ava'
+import sao from 'sao'
 
-const template = path.join(__dirname, '..');
+const template = path.join(__dirname, '..')
 
 const getPkgFields = pkg => {
-  pkg = JSON.parse(pkg);
-  delete pkg.description;
-  return pkg;
-};
+  pkg = JSON.parse(pkg)
+  delete pkg.description
+  return pkg
+}
 
 const verifyPkg = async (t, answers) => {
-  const stream = await sao.mockPrompt(template, answers);
+  const stream = await sao.mockPrompt(template, answers)
 
-  const pkg = stream.fileContents('package.json');
-  t.snapshot(stream.fileList, 'Generated files');
-  t.snapshot(getPkgFields(pkg), 'package.json');
-};
+  const pkg = stream.fileContents('package.json')
+  t.snapshot(stream.fileList, 'Generated files')
+  t.snapshot(getPkgFields(pkg), 'package.json')
+}
 
 test('defaults', async t => {
   await verifyPkg(t, {
     name: 'san',
-  });
-});
+  })
+})
 
 test('custom description', async t => {
   await verifyPkg(t, {
     name: 'san',
     description: 'description',
-  });
-});
+  })
+})
